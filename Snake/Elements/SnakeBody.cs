dd5f5308121efe0f214b982d3fd55527952af66b
Kristýna Moravcová <kmoravcova1997@gmail.com>
2022-04-21 22:01:46 +0200

    created snake body

---
+++ Snake/Elements/SnakeBody.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snake.Elements
+{
+    enum EDirection
+    { 
+        up,
+        right,
+        down,
+        left
+    }
+    class SnakeBody
+    {
+        public List<Point> BodyParts { get; private set; }
+        public EDirection Direction { get; private set; }
+        public int Lenght => BodyParts.Count();
+
+        public SnakeBody(Point inicialPoint, EDirection initialDirection)
+        {
+            this.BodyParts.Add(inicialPoint);
+            this.Direction = initialDirection;            
+        }        
+        public void ChangeDirection(EDirection newDirection)
+        {
+            this.Direction = newDirection;
+        }
+        public void MoveASnake(Point newPosition)
+        {
+            this.BodyParts.Insert(0, newPosition);
+            this.BodyParts.Remove(BodyParts.Last());
+        }
+    }
+}
